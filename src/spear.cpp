0e7aebea0e2513a0b2f078ba4b484d7ace135c1d
tfgl <tafanelgil@gmail.com>
2023-03-09 13:25:48 +0100

    callable from anywhere inside a project tree

--- src/spear.cpp
+++ src/spear.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cstdlib>
+#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <iterator>
@@ -13,28 +14,35 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <vector>
 
 #include "toml.h"
 #include "man.h"
 
 namespace fs = std::filesystem;
+using std::string;
+using std::vector;
+using strvec = vector<string>;
+using path = fs::path;
 
-char cc[] = "g++";
-std::string xdg_config_home(getenv("XDG_CONFIG_HOME"));
-std::string xdg_data_home(getenv("XDG_DATA_HOME"));
+string cc = "g++";
 
-std::string glb_config_path = xdg_config_home.compare("") == 0
-  ? std::string(getenv("HOME")) + "/.config/spear.toml"
-  : xdg_config_home + "/spear.toml";
+path xdg_config_home(getenv("XDG_CONFIG_HOME"));
+path xdg_data_home(getenv("XDG_DATA_HOME"));
+path home(getenv("HOME"));
+
+path glb_config_path = fs::exists(xdg_config_home)
+  ? xdg_config_home / "spear.toml"
+  : home / ".config"/"spear.toml";
 
 // TODO create dir & file when installing
-std::string libs_config_path = xdg_data_home.compare("") == 0
-  ? std::string(getenv("HOME")) + "/.local/share/spear/libs.toml"
-  : xdg_data_home + "/spear/libs.toml";
+path libs_config_path = fs::exists(xdg_data_home)
+  ? xdg_data_home / "spear"/"libs.toml"
+  : home / ".local"/"share"/"spear"/"libs.toml";
 
-auto glb_config = toml::parse_file(glb_config_path);
+auto glb_config = toml::parse_file(glb_config_path.string());
 
-std::string hello_world_prgm = 
+string hello_world_prgm = 
 R"(#include <iostream>
 
 int main(int argc, char* argv[]) {
@@ -42,14 +50,34 @@ int main(int argc, char* argv[]) {
   return 0;
 })";
 
-std::vector<char*> get_dependencies() {
-  std::vector<char*> deps_arg;
+path root = fs::current_path();
 
-  auto config = toml::parse_file("../spear.toml");
+void find_root() {
+  while ( !fs::exists(root / "spear.toml") && root.root_path() != root ) {
+    root = root.parent_path();
+  }
+}
+
+void m_execvp(strvec cmd) {
+  char** c_cmd = (char**)malloc((cmd.size() + 1) * sizeof(char*));
+
+  for (size_t i=0; i<cmd.size(); i++) {
+    c_cmd[i] = (char*)malloc(cmd[i].size() * sizeof(char) + 1);
+    strcpy(c_cmd[i], cmd[i].c_str());
+  }
+  c_cmd[cmd.size()] = NULL;
+
+  execvp(c_cmd[0], c_cmd);
+}
+
+strvec get_dependencies() {
+  strvec deps_arg;
+
+  auto config = toml::parse_file((root / "spear.toml").string());
   auto dependencies = config.at_path("dependencies");
   dependencies.as_table()->for_each([&deps_arg](toml::table& dependency) {
     if( auto arr = dependency.at_path("command").as_array() ) {
-      arr->for_each([&deps_arg](toml::value<std::string>& arg) {
+      arr->for_each([&deps_arg](toml::value<string>& arg) {
         deps_arg.push_back(arg->data());
       });
     }
@@ -60,50 +88,52 @@ std::vector<char*> get_dependencies() {
 
 void new_project(const int argc, char* argv[]) {
   CHECK((argc < 1), man::new_)
-  std::string project_name(argv[1]);
+  fs::path project_name(argv[1]);
+
   if (fs::exists(project_name)) {
     std::cout << "project name already taken" << std::endl;
     return;
   }
 
-  std::string directories[] = {
-    project_name,
-    std::string(project_name) + "/src",
-  };
+  // create tree structure
+  fs::create_directory(project_name);
+  fs::create_directory(project_name / "src");
 
-  for (auto dir: directories)
-    fs::create_directory(dir);
-
-  std::ofstream f_main(project_name + "/src/main.cpp");
+  // write default hello world
+  std::ofstream f_main(project_name / "src" / "main.cpp");
   f_main << hello_world_prgm << std::endl;
   f_main.close();
 
-  std::ofstream f_spear(project_name + "/spear.toml");
+  // write default spear toml config
+  std::ofstream f_spear(project_name / "spear.toml");
   f_spear << "[project]\n"
           << "name = '" << argv[1] << "'\n"
           << "version = '0.1.0'\n"
           << "authors = ["<< glb_config.at_path("user") <<"]\n"
           << "\n"
           << "[dependencies]\n";
-
   f_spear.close();
 
+  // create git repo
+  std::cout << "create git repo" << std::endl;
   pid_t pid = fork();
   if (pid == 0) {
-    char* args[] = {"git", "init", project_name.data(), NULL};
-    execvp(args[0], args);
+    m_execvp({"git", "init", project_name.string()});
+    exit(0);
   } else {
     wait(NULL);
-    auto of = std::ofstream(project_name + "/.gitignore");
-    of << "target/"
+
+    std::ofstream of(project_name / ".gitignore");
+    of << "target"
        << std::endl;
+    of.close();
   }
 }
 
-std::vector<char*> build_objects(std::string base_output_dir, std::vector<char*>& cmd_args) {
-  fs::current_path("src/");
-  fs::path output_dir("../"+base_output_dir);
-  std::vector<char*> args = {cc};
+strvec build_objects(string base_output_dir, strvec& cmd_args) {
+  fs::current_path(root / "src");
+  path output_dir(root / base_output_dir);
+  strvec args = {cc};
   auto dependencies = get_dependencies();
   args.insert(args.end(), dependencies.begin(), dependencies.end());
 
@@ -114,67 +144,66 @@ std::vector<char*> build_objects(std::string base_output_dir, std::vector<char*>
       continue;
     }
 
-    std::string white_list[] = {".c", ".cpp", "c++", "cxx"};
+    string white_list[] = {".c", ".cpp", "c++", "cxx"};
     if (std::find(std::begin(white_list), std::end(white_list),
                   file.path().extension().string().c_str()) == std::end(white_list))
       continue;
 
-    std::string object_file = output_dir.string().append(file.path().parent_path().append(file.path().stem().string().append(".o")));
-    args.push_back(strdup(object_file.c_str()));
+    string object_file = output_dir / file.path().parent_path() / file.path().stem().concat(".o");
+    args.push_back(object_file);
 
-    auto object_path = fs::path{object_file};
+    auto object_path = path{object_file};
     if( fs::exists(object_path) && fs::last_write_time(object_path) > fs::last_write_time(file) )
       continue;
 
-    std::cout << "file: " << file.path().string() << std::endl;
-
     pid_t pid = fork();
     if (pid == 0) {
-      cmd_args.push_back(object_file.data());
-      cmd_args.push_back(file.path().string().data());
-      cmd_args.push_back(NULL);
+      cmd_args.push_back(object_file);
+      cmd_args.push_back(file.path());
 
-      execvp(cmd_args[0], cmd_args.data());
+      m_execvp(cmd_args);
       exit(0);
     } else {
       wait(NULL);
     }
   }
 
+  fs::current_path(root);
   args.push_back("-o");
   return args;
 }
 
 void build(const int argc, char* argv[]) {
-  std::string project_name = toml::parse_file("spear.toml").at_path("project.name").value_or("");
-  std::string target_dir;
-  std::string target;
-  std::vector<char*> build_args;
+  string project_name = toml::parse_file( (root / "spear.toml").string() )
+                              .at_path("project.name")
+                              .value_or("");
+  path target_dir(root / "target");
+  path target;
+  strvec build_args;
 
-  fs::create_directory("target/");
+  fs::create_directory(target_dir);
 
-  if (argc >= 2 && std::string(argv[1]).compare("release") == 0) {
-    target_dir = "target/release/";
+  if (argc >= 2 && string(argv[1]) == "release") {
+    target_dir /= "release";
     build_args = {cc, "-c", "-O3", "-std=c++20", "-o"};
   }
   else {
-    target_dir = "target/debug/";
+    target_dir /= "debug";
     build_args = {cc, "-c", "-Og", "-g3", "-Wall", "-std=c++20", "-o"};
   }
 
   fs::create_directory(target_dir);
-  target = target_dir + project_name.c_str();
+  target = target_dir / project_name;
 
   auto args = build_objects(target_dir, build_args);
-  args.push_back(strdup(("../" + target).c_str()));
+  args.push_back(root / target);
 
   std::cout << "LINKING" << std::endl;
   for(auto& arg: args)
     std::cout << arg << " ";
   std::cout << std::endl;
 
-  args.push_back(NULL);
-  execvp(args[0], args.data());
+  m_execvp(args);
 }
 
 void run(const int argc, char* argv[]) {
@@ -187,16 +216,20 @@ void run(const int argc, char* argv[]) {
     wait(NULL);
   }
 
-  std::vector<char*> command;
+  strvec command;
+  string project_name = toml::parse_file( (root / "spear.toml").string() )
+                                        .at_path("project.name")
+                                        .value_or("");
 
-  std::string project_name = toml::parse_file("spear.toml").at_path("project.name").value_or("");
-  std::string target = (argc >= 2 && std::string("release").compare(argv[1]) == 0) ? "target/release/" + project_name : "target/debug/" + project_name;
-  command.push_back(target.data());
+  fs::path target = (argc >= 2 && string(argv[1]) == "release")
+    ? root / "target" / "release" / project_name
+    : root / "target" / "debug" / project_name;
+  command.push_back(target);
 
   int with_position = 0;
-  if( argc >= 2 && std::string("with").compare(argv[1]) == 0 ) // spear run with a b c d
+  if( argc >= 2 && string(argv[1]) == "with" ) // spear run with a b c d
     with_position = 2;
-  if( argc >= 3 && std::string("with").compare(argv[2]) == 0 ) // spear run release/debug with a b c d
+  if( argc >= 3 && string(argv[2]) == "with" ) // spear run release/debug with a b c d
     with_position = 3;
 
   if( with_position != 0 ) {
@@ -205,13 +238,12 @@ void run(const int argc, char* argv[]) {
     }
   }
 
-  command.push_back(NULL);
   std::cout << "RUNING" << std::endl;
-  execvp(command[0], command.data());
+  m_execvp(command);
 }
 
 void clean() {
-  fs::remove_all("target");
+  fs::remove_all(root / "target");
 }
 
 void package(const int argc, char* argv[]) {
@@ -225,13 +257,13 @@ void fetch(const int argc, char* argv[]) {
 void add(const int argc, char* argv[]) {
   CHECK((argc < 2), man::add)
 
-  std::string lib_name;
-  std::string lib_version;
+  string lib_name;
+  string lib_version;
   bool is_localy_provided = false;
 
   int max = argc > 3 ? 3 : argc;
   for (int i=1; i<max; i++) {
-    if (std::string("--local").compare(argv[i]) == 0) {
+    if (string("--local").compare(argv[i]) == 0) {
       is_localy_provided = true;
     }
     else if (lib_name.empty()) {
@@ -242,17 +274,17 @@ void add(const int argc, char* argv[]) {
     }
   }
 
-  auto libs_cfg = toml::parse_file(libs_config_path);
+  auto libs_cfg = toml::parse_file(libs_config_path.string());
   auto lib_cfg = libs_cfg[lib_name];
   auto commands = lib_cfg["command"];
 
   auto config = toml::parse_file("spear.toml");
-  std::string cfg_string =
+  string cfg_string =
                            "version = '"+lib_version+"'\n"
                            "command = [";
 
-  commands.as_array()->for_each([&cfg_string](toml::value<std::string> el) {
-    cfg_string.append("'" + std::string(el->data()) + "'," );
+  commands.as_array()->for_each([&cfg_string](toml::value<string> el) {
+    cfg_string.append("'" + string(el->data()) + "'," );
   });
   cfg_string.back() = ']';
 
@@ -268,26 +300,38 @@ void add(const int argc, char* argv[]) {
 }
 
 void spear(const int argc, char* argv[]) {
-  CHECK((argc < 2 || std::string("help").compare(argv[1]) == 0), man::spear)
-  std::string argv1(argv[1]);
+  CHECK((argc < 2), man::spear)
+  string argv1(argv[1]);
 
-  if (argv1.compare("new") == 0) {
+  if (argv1 == "new") {
     new_project(argc - 1, argv + 1);
-  } else if (argv1.compare("run") == 0) {
-    run(argc - 1, argv+1);
-  } else if (argv1.compare("build") == 0) {
-    build(argc - 1, argv+1);
-  } else if (argv1.compare("clean") == 0) {
-    clean();
-  } else if (argv1.compare("package") == 0) {
-    package(argc - 1, argv+1);
-  } else if (argv1.compare("fetch") == 0) {
-    fetch(argc - 1, argv+1);
-  } else if (argv1.compare("add") == 0) {
-    add(argc - 1, argv+1);
-  } else if (argv1.compare("get_name") == 0) {
-    std::cout << toml::parse_file("spear.toml").at_path("project.name") << std::endl;
-  } else {
-    std::cout << man::spear << std::endl;
+    return;
   }
+
+  find_root();
+
+  if (argv1 == "run")
+    run(argc - 1, argv+1);
+
+  else if (argv1 == "build")
+    build(argc - 1, argv+1);
+
+  else if (argv1 == "clean")
+    clean();
+
+  else if (argv1 == "package")
+    package(argc - 1, argv+1);
+
+  else if (argv1 == "fetch")
+    fetch(argc - 1, argv+1);
+
+  else if (argv1 == "add")
+    add(argc - 1, argv+1);
+
+  else if (argv1 == "get_name")
+    std::cout << toml::parse_file("spear.toml").at_path("project.name") << std::endl;
+
+  else
+    std::cout << man::spear << std::endl;
+  
 }
